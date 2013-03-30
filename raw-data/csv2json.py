6513b2315aef11a0456299ddf119c44b97f2a013
Eric Bloch <eric.bloch@gmail.com>
2013-03-29 17:12:06 -0700

    latest csv2json and latest csvs

--- raw-data/csv2json.py
+++ raw-data/csv2json.py
@@ -4,9 +4,12 @@ import os
 import sys
 import csv
 import json
+import time
+import datetime
+from datetime import timedelta
 
 # Keynotes
-#KEYNOTE TITLE,ABSTRACT,SPEAKER NAME,SPEAKER TITLE,SPEAKER COMPANY,SPEAKER BIOGRAPHY
+#SESSION TITLE,ABSTRACT,SPEAKER NAME,SPEAKER TITLE,SPEAKER COMPANY,SPEAKER BIOGRAPHY
 
 # Solution Track
 # SESSION TITLE,HOUR TITLE,SESSION ABSTRACT,AUDIENCE TAKE-AWAY,SPEAKER FIRST,SPEAKER LAST,JOB TITLE,COMPANY NAME,EMAIL ADDRESS,PRESENTATION FORMAT,PHONE,STATUS,BIOGRAPHY,DATE OF SESSION,TIME OF SESSION,ROOM,TRACK,SESSION TAG,SESSION OWNER,PHOTO,CO-PRESENTER FIRST,CO-PRESENTER LAST,CO-PRESENTER JOB TITLE,CO-PRESENTER COMPANY,CO-PRESENTER BIOGRAPHY,CO-PRESENTER #2 FIRST,CO-PRESENTER #2 LAST,CO-PRESENTER #2 JOB TITLE,CO-PRESENTER #2 COMPANY,CO-PRESENTER #2 BIOGRAPHY,SESSION NOTES
@@ -59,6 +62,62 @@ tracks = {
     'Tech and Lightning' : 'technical'
 }
 
+speakers = dict()
+
+for c in ['Partners', 'Solutions Track', 'Tech and Lightning']:
+    with open("MLW13_Master Session Listing - " + c + ".csv", 'rb') as csvfile:
+        creader = csv.DictReader(csvfile)
+        for row in creader:
+            name = row['SPEAKER FIRST'].strip() + ' ' + row['SPEAKER LAST'].strip()
+            if (not(name in speakers)) :
+                speakerID = speakerID + 1
+                speaker_file = open('speakers/' + str(speakerID) + ".json" , 'w')
+                speaker = dict()
+                speaker['type'] = 'speaker'
+                speaker['name'] = name
+                speaker['affiliation'] = row['COMPANY NAME']
+                speaker['bio'] = row['BIOGRAPHY']
+                speaker['email'] = row['EMAIL ADDRESS']
+                speaker['position'] = row['JOB TITLE']
+                speaker['id'] = "" + str(speakerID)
+                speaker_file.write(json.dumps(speaker, sort_keys=True,
+                  indent=4, separators=(',', ': ')))
+                speakers[name] = speaker
+
+            if row['CO-PRESENTER FIRST'] :
+                name = row['CO-PRESENTER FIRST'].strip() + ' ' + row['CO-PRESENTER LAST'].strip()
+                if (not(name in speakers)) :
+                    speakerID = speakerID + 1
+                    speaker_file = open('speakers/' + str(speakerID) + ".json" , 'w')
+                    speaker = dict()
+                    speaker['type'] = 'speaker'
+                    speaker['name'] = name
+                    speaker['affiliation'] = row['CO-PRESENTER COMPANY']
+                    speaker['bio'] = row['CO-PRESENTER BIOGRAPHY']
+                    speaker['email'] = ''  #TODO
+                    speaker['position'] = row['CO-PRESENTER JOB TITLE']
+                    speaker['id'] = "" + str(speakerID)
+                    speaker_file.write(json.dumps(speaker, sort_keys=True,
+                        indent=4, separators=(',', ': ')))
+                    speakers[name] = speaker
+
+            if row['CO-PRESENTER #2 FIRST'] :
+                name = row['CO-PRESENTER #2 FIRST'].strip() + ' ' + row['CO-PRESENTER #2 LAST'].strip()
+                if (not(name in speakers)) :
+                    speakerID = speakerID + 1
+                    speaker_file = open('speakers/' + str(speakerID) + ".json" , 'w')
+                    speaker = dict()
+                    speaker['type'] = 'speaker'
+                    speaker['name'] = name
+                    speaker['affiliation'] = row['CO-PRESENTER #2 COMPANY']
+                    speaker['bio'] = row['CO-PRESENTER #2 BIOGRAPHY']
+                    speaker['email'] = ''  #TODO
+                    speaker['position'] = row['CO-PRESENTER #2 JOB TITLE']
+                    speaker['id'] = "" + str(speakerID)
+                    speaker_file.write(json.dumps(speaker, sort_keys=True,
+                        indent=4, separators=(',', ': ')))
+                    speakers[name] = speaker
+
 for c in ['Partners', 'Solutions Track', 'Tech and Lightning']:
     with open("MLW13_Master Session Listing - " + c + ".csv", 'rb') as csvfile:
         creader = csv.DictReader(csvfile)
@@ -77,64 +136,54 @@ for c in ['Partners', 'Solutions Track', 'Tech and Lightning']:
             session['featured'] = False
             session['id'] = "" + str(sessionID)
             date = row['DATE OF SESSION']
-            time = row['TIME OF SESSION']
-            if date and time:
-                day = '10'
-                hour = '01'
-                minute = '00'
-                session['startTime'] = '2013-04-' + day + 'T' + hour + ":" + minute + ':00'
-                session['endTime'] = '2013-04-' + day + 'T' + hour + ":" + minute + ':00'
+            s_time = row['TIME OF SESSION']
+            if date and s_time:
+                if '-' in date:
+                    day = int(date.split('-')[1])
+                else:
+                    day = int(date.split()[1])
+                t = s_time.split(':')
+                hour = int(t[0])
+                goo = t[1].split()
+                minute = int(goo[0])
+                ampm = goo[1]
+                if (ampm == 'PM'): 
+                    hour += 12
+
+                start = datetime.datetime(year=2013, month=04, day=day, hour=hour, minute=minute)
+                end = start + timedelta(minutes=50)
+
+                start_str = start.strftime("%Y-%m-%dT%H:%M:%S")
+                end_str = end.strftime("%Y-%m-%dT%H:%M:%S")
+
+                
+                session['startTime'] = start_str
+                session['endTime'] = end_str
             else:
-                print "Skipping"
+                print "Skipping " + session['title']
                 continue
 
-            session['speakerNames'] = [ row['SPEAKER FIRST'] + ' ' + row['SPEAKER LAST'] ]
+            name = row['SPEAKER FIRST'].strip() + ' ' + row['SPEAKER LAST'].strip() 
+            coname = row['CO-PRESENTER FIRST'].strip() + ' ' + row['CO-PRESENTER LAST'].strip()
+            coname2 = row['CO-PRESENTER #2 FIRST'].strip() + ' ' + row['CO-PRESENTER #2 LAST'].strip()
 
-            speakerID = speakerID + 1
-            speaker_file = open('speakers/' + str(speakerID) + ".json" , 'w')
-
-            speaker = dict()
-            speaker['type'] = 'speaker'
-            speaker['name'] = row['SPEAKER FIRST'] + ' ' + row['SPEAKER LAST']
-            speaker['affiliation'] = row['COMPANY NAME']
-            speaker['bio'] = row['BIOGRAPHY']
-            speaker['email'] = row['EMAIL ADDRESS']
-            speaker['position'] = row['JOB TITLE']
-            speaker['id'] = "" + str(speakerID)
-            speaker_file.write(json.dumps(speaker))
-
+            speakerID = speakers[name]['id']
+            session['speakerNames'] = [ name ]
             session['speakerIds'] = [ "" + str(speakerID) ]
 
-            if row['CO-PRESENTER FIRST'] :
-                speakerID = speakerID + 1
-                speaker_file = open('speakers/' + str(speakerID) + ".json" , 'w')
-                speaker = dict()
-                speaker['type'] = 'speaker'
-                speaker['name'] = row['CO-PRESENTER FIRST'] + ' ' + row['CO-PRESENTER LAST']
-                speaker['affiliation'] = row['CO-PRESENTER COMPANY']
-                speaker['bio'] = row['CO-PRESENTER BIOGRAPHY']
-                speaker['email'] = ''
-                speaker['position'] = row['CO-PRESENTER JOB TITLE']
-                speaker['id'] = "" + str(speakerID)
-                speaker_file.write(json.dumps(speaker))
-                session['speakerNames'].append(row['CO-PRESENTER FIRST'] + ' ' + row['CO-PRESENTER LAST'])
+            if row['CO-PRESENTER FIRST'] and (coname != name) :
+                name = coname
+                session['speakerNames'].append(name)
+                speakerID = speakers[name]['id']
                 session['speakerIds'].append("" + str(speakerID))
 
-            if row['CO-PRESENTER #2 FIRST'] :
-                speakerID = speakerID + 1
-                speaker_file = open('speakers/' + str(speakerID) + ".json" , 'w')
-                speaker = dict()
-                speaker['type'] = 'speaker'
-                speaker['name'] = row['CO-PRESENTER #2 FIRST'] + ' ' + row['CO-PRESENTER #2 LAST']
-                speaker['affiliation'] = row['CO-PRESENTER #2 COMPANY']
-                speaker['bio'] = row['CO-PRESENTER #2 BIOGRAPHY']
-                speaker['email'] = ''
-                speaker['position'] = row['CO-PRESENTER #2 JOB TITLE']
-                speaker['id'] = "" + str(speakerID)
-                speaker_file.write(json.dumps(speaker))
-                session['speakerNames'].append(row['CO-PRESENTER #2 FIRST'] + ' ' + row['CO-PRESENTER #2 LAST'])
-                session['speakerIds'].append(speakerID)
+            if row['CO-PRESENTER #2 FIRST'] and (coname2 != name) :
+                name = coname2
+                session['speakerNames'].append(name)
+                speakerID = speakers[name]['id']
+                session['speakerIds'].append("" + str(speakerID))
 
             session_file = open('sessions/' + str(sessionID) + ".json" , 'w')
-            session_file.write(json.dumps(session))
+            session_file.write(json.dumps(session, sort_keys=True,
+                  indent=4, separators=(',', ': ')))
 
